d4556c40f081059f9d54d5ca95fcb1c0b0f262ef
rob011235 <rgarner011235@gmail.com>
2022-04-07 14:58:06 -0600

    finished planet scene

---
+++ Assets/Scripts/Devices/PlanetaryDevice.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+
+//This class is intended to be used to bring up a gui with maps of the planet, moon or ship so the player can pick various options
+public class PlanetaryDevice : BaseDevice
+{
+    public override void Operate()
+    {
+        Debug.Log($"{this.gameObject.name} Operated");
+    }
+}

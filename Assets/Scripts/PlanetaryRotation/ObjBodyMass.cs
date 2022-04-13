64a541c88ebbc9582ed20f40e1018da6640b4219
Michael Mojarro <77815234+mmojarro@users.noreply.github.com>
2022-04-12 20:07:21 -0600

    Initial PlanetaryMotion Push
    Added (Solar System) PlanetaryMotion scene and created prefab of solar system. Planets rotate around star.

---
+++ Assets/Scripts/PlanetaryRotation/ObjBodyMass.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjBodyMass : MonoBehaviour
+{
+    public List<Transform> planets;
+    public float mass = 100f;
+}

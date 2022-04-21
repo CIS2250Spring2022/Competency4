9c3c695a2d29bc7364d056b04a9a2585e789ce55
Michael Mojarro <77815234+mmojarro@users.noreply.github.com>
2022-04-20 21:40:28 -0600

    Revert "MainMenu/StarterScene Initial Commit"
    This reverts commit 52bfb08bee59c77f17cc67f81173e22eda6838cf.

--- Assets/Scripts/PlanetaryRotation/ObjBodyMass.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ObjBodyMass : MonoBehaviour
-{
-    public List<Transform> planets;
-    public float mass = 100f;
-}
